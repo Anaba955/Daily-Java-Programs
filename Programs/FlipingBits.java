c89769be3b71e00c0506fea6db5404b18d1afb12
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2024-05-28 01:12:55 +0530

    Create FlipingBits.java

---
+++ Programs/FlipingBits.java
@@ -0,0 +1,34 @@
+/* You will be given a list of 32 bit unsigned integers. 
+Flip all the bits (1 -> 0 and 0 -> 1) and return the result as an unsigned integer.
+*/
+
+class Result {
+    public static long flippingBits(long n) {
+        return ~n & 0xFFFFFFFFL;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                long result = Result.flippingBits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
