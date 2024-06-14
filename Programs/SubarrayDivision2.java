035a94eb21f80d293c8c6725ebff4c7da3b74d08
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2024-06-14 22:00:35 +0530

    Create SubarrayDivision2.java

---
+++ Programs/SubarrayDivision2.java
@@ -0,0 +1,48 @@
+/*Two children, Lily and Ron, want to share a chocolate bar. Each of the squares has an integer on it.
+Lily decides to share a contiguous segment of the bar selected such that:
+The length of the segment matches Ron's birth month, and,
+The sum of the integers on the squares is equal to his birth day.
+Determine how many ways she can divide the chocolate.*/
+
+class Result {
+
+    public static int birthday(List<Integer> s, int d, int m) {
+        int count = 0, sum = 0;
+        for(int i = 0; i < s.size() - m + 1; i++) {
+            sum = 0;
+            for(int j = i; j < i+m; j++) {
+                sum += s.get(j);
+            }
+            if(sum == d)    count++;
+        }
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
