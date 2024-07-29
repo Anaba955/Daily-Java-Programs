11667297af885fedb30f73c02eb86ee13347fe50
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2024-07-29 12:47:49 +0530

    Update GoodPairs.java

--- Programs/GoodPairs.java
+++ Programs/GoodPairs.java
@@ -4,7 +4,7 @@ class Solution {
     public int numIdenticalPairs(int[] nums) {
         int count = 0;
         HashMap<Integer, Integer> hm = new HashMap<>();
-        for(int i = 0; i < nums; i++) {
+        for(int i = 0; i < nums.length; i++) {
             count += hm.getOrDefault(nums[i], 0);  // Available like minded guests in the hall
             hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);  // adding new guest
         }
