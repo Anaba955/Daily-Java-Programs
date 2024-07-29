7fc26642c6fcfbed006bfe61b647fc98a04e01bf
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2024-07-29 12:46:01 +0530

    Update GoodPairs.java

--- Programs/GoodPairs.java
+++ Programs/GoodPairs.java
@@ -5,8 +5,8 @@ class Solution {
         int count = 0;
         HashMap<Integer, Integer> hm = new HashMap<>();
         for(int i = 0; i < nums; i++) {
-            count += hm.getOrDefault(nums[i], 0);
-            hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
+            count += hm.getOrDefault(nums[i], 0);  // Available like minded guests in the hall
+            hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);  // adding new guest
         }
         return count;
     }
