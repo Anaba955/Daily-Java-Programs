ac6105965585d789e10ef703dcd190e0afdb942a
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2024-10-13 20:16:43 +0530

    Create AlternateAdd_Sub.java

---
+++ Programs/AlternateAdd_Sub.java
@@ -0,0 +1,15 @@
+class UserMainCode
+{
+	public int AddSub(int input1, input2) {
+		int result1 = input1;
+		boolean sub = iput2 == 1 ? true: false;
+		for(int i = input-1; i > 0; i--) {
+		    if(sub)
+		        result -= i;
+		    else 
+		        result += i;
+		    sub != sub;
+		}
+		return result;
+	}
+}
