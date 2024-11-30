b1b15c4554258fae2636cf18cffdad9ee5f6435d
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2024-11-30 20:53:26 +0530

    Create rotateArr.java

---
+++ Programs/rotateArr.java
@@ -0,0 +1,19 @@
+//https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/rotate-array-by-n-elements-1587115621
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+        d %= arr.length;
+        reverseArr(arr, 0, d-1);
+        reverseArr(arr, d, arr.length-1);
+        reverseArr(arr, 0, arr.length-1);
+    }
+    static void reverseArr(int[] arr, int start, int end) {
+        while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
