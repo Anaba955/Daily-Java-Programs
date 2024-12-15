432cfe11e6f82e2af132dce60c6a1db37207d759
Anaba Abbas <129278969+Anaba955@users.noreply.github.com>
2024-12-15 23:46:07 +0530

    Update SingleTransactionProfit.java

--- Programs/SingleTransactionProfit.java
+++ Programs/SingleTransactionProfit.java
@@ -3,7 +3,7 @@ class Solution {
         int maxProfit = 0, minPrice=Integer.MAX_VALUE;
         for(int price : prices) {
             if(price < minPrice)
-                minPrice = price;
+                minPrice = price; //keeping track of min price encountered
             else if(price - minPrice > maxProfit)
                 maxProfit = price - minPrice;
         }
